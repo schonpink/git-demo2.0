0efa533604b1a9a6a06b90f9c66ca683a2fd4727
Svetlana Obrancova <obrancova.s@gmail.com>
2023-06-05 14:55:07 +0200

    2 update App

--- App.java
+++ App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Открытие магазина ожидается через 3 недели");
+        System.out.println("Открытие магазина ожидается через 2 недели");
     }
 }
